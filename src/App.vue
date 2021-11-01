5b488d1dc36a4aa0c9fa13a618ac3a7f6f0fea66
Giovanni Solano <giovannisolano@MacBook-Pro-de-Giovanni.local>
2021-10-31 21:11:28 -0600

    Fundamentos, primera version

---
+++ src/App.vue
@@ -0,0 +1,32 @@
+<template>
+  <!-- <img alt="Vue logo" src="./assets/logo.png"> -->
+
+  <!-- <Counter :start="100" /> -->
+  <Indecision />
+  <!-- <Counter v-bind:start="15"/> -->
+
+</template>
+
+<script>
+
+import Indecision from "@/components/Indecision.vue";
+// import Counter from "@/components/Counter.vue";
+
+export default {
+  name: 'App',
+  components: {
+    Indecision
+  }
+}
+</script>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
